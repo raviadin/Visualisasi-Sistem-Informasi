b6861e78a663ff92728f305483da0a22a8940f32
raviadin <raviadinnn@gmail.com>
2023-07-04 11:46:59 +0700

    update side, user, admin blade

--- resources/views/users/admin.blade.php
+++ resources/views/users/admin.blade.php
@@ -23,7 +23,7 @@
                     <div class="m-4">
                         <!-- Button trigger modal -->
                         <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-                            <i class="fas fa-user-plus"></i> Add Users
+                            <i class="fas fa-user-plus"></i> Add Admin
                         </button>
 
 
